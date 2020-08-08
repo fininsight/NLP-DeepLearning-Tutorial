ffd514132bfec844621614b13a2d77c9b5a16fea
daniel <daniel.lee@fininsight.co.kr>
2020-08-09 08:42:24 +0900

    word2vec class 파일 추가

---
+++ 02_Prac_1_Word2Vec.py
@@ -0,0 +1,157 @@
+from collections import defaultdict
+import numpy as np 
+
+class OneHotEncoder() :
+  def __init__(self, docs) :
+    # 고유 단어와 인덱스를 매칭시켜주는 사전 생성
+    self.w2i = defaultdict(lambda:len(self.w2i))
+    [self.w2i[w] for d in docs for w in d]
+    self.i2w = {v:k for k,v in self.w2i.items()}
+
+  def _get_one_hot_vector(self, w):
+    v = [0]*len(self.w2i)
+    v[self.w2i[w]] = 1
+    return v
+
+  def encode(self, docs) :
+    ret = []
+    for d in docs :
+      tmp = []
+      for w in d :
+        tmp.append(self._get_one_hot_vector(w))
+      ret.append(tmp)
+    return ret
+
+  def decode(self, v) :
+    return self.i2w[v.index(1)]
+
+import numpy as np
+from sklearn.manifold import TSNE
+from tqdm.notebook import tqdm as tqdm
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+class Word2Vec() :
+  def __iniit__(self):
+    pass
+
+  def _slide_window(self, encoded_docs, win_size = 2) :
+    ret = []
+
+    for d in encoded_docs : 
+      win_doc = []
+      for i, w in enumerate(d) :
+        s, e = max(0, i - win_size), min(len(d), i + win_size)
+        context = d[s:e+1]
+        center = context.pop(i-s)
+        win_doc.append((center, context))
+      ret.append(win_doc)
+    return ret
+
+  def _softmax(self, output) :
+    return np.exp(output) / np.sum(np.exp(output))
+
+  #SGD to backpropagate errors 
+  def _backpropagation(self, W1, W2, hidden, predict, center, context, learning_rate) :
+      err = (self._softmax(predict) - context).sum(axis=0)
+
+      delta_W2 = np.outer(hidden, err)
+      delta_W1 = np.outer(center, np.dot(W2, err))
+
+      W1 = W1 - learning_rate * delta_W1
+      W2 = W2 - learning_rate * delta_W2
+
+      return W1, W2
+
+  def embedding(self, docs, method = 'sg', win_size = 2, embedding_size = 5, learning_rate = 0.001, epoch = 10 ) :
+    if method == 'sg' :
+      return self._skipgram(docs, win_size, embedding_size, learning_rate, epoch)
+    elif method == 'cbow' :
+      return self._cbow(docs, win_size, embedding_size, learning_rate, epoch)
+
+  def _cbow(self, docs, win_size = 2, embedding_size = 5, learning_rate = 0.001, epoch = 10) :
+    tokenized_docs = [d.split() for d in docs]
+    ohe = OneHotEncoder(tokenized_docs)
+    encoded_docs = ohe.encode(tokenized_docs)
+    
+    onehot_size = len(encoded_docs[0][0])
+    W1 = np.random.rand(onehot_size, embedding_size)
+    W2 = np.random.rand(embedding_size, onehot_size)
+
+    sliding_docs = self._slide_window(encoded_docs, win_size)
+    for i in tqdm(range(epoch), desc='word embedding') :
+      for d in  sliding_docs:
+        for center, context in d : 
+          #이 부분 고쳐햐암
+          hidden = np.dot(context, W1)
+          predict = np.dot(hidden, W2)
+          W1, W2 = self._backpropagation(W1, W2, hidden, predict, context, center, learning_rate)
+      #self.loss += C*np.log(np.sum(np.exp(self.u))) 
+    
+    self.i2w = ohe.i2w
+    self.wv_ = {self.i2w[i]:list(we) for i, we in enumerate(W1)}
+    self.word_vectors = W1
+
+    return self.wv_
+
+    pass
+
+  def _skipgram(self, docs, win_size = 2, embedding_size = 5, learning_rate = 0.001, epoch = 10) :
+    tokenized_docs = [d.split() for d in docs]
+    ohe = OneHotEncoder(tokenized_docs)
+    encoded_docs = ohe.encode(tokenized_docs)
+    
+    onehot_size = len(encoded_docs[0][0])
+    W1 = np.random.rand(onehot_size, embedding_size)
+    W2 = np.random.rand(embedding_size, onehot_size)
+
+    sliding_docs = self._slide_window(encoded_docs, win_size)
+    for i in tqdm(range(epoch), desc='word embedding') :
+      for d in  sliding_docs:
+        for center, context in d : 
+          hidden = np.dot(center, W1)
+          predict = np.dot(hidden, W2)
+          W1, W2 = self._backpropagation(W1, W2, hidden, predict, center, context, learning_rate)
+      #self.loss += C*np.log(np.sum(np.exp(self.u))) 
+    
+    self.i2w = ohe.i2w
+    self.wv_ = {self.i2w[i]:list(we) for i, we in enumerate(W1)}
+    self.word_vectors = W1
+
+    return self.wv_
+
+  def most_similar(self, word, n = 3) :
+    v = self.wv_[word]
+    similarity = np.dot(v, self.word_vectors.T)
+    return [(self.i2w[i], similarity[i], self.word_vectors[i]) for i in similarity.argsort()[::-1][:n]]
+
+  def visualize(self, word, n = 3) :
+    wvs = self.most_similar(word, n)
+
+    # 기본 글꼴 변경
+    mpl.font_manager._rebuild()
+    mpl.pyplot.rc('font', family='NanumBarunGothic')
+
+    words = [item[0] for item in wvs]
+    wvs = [item[2] for item in wvs]
+
+    tsne = TSNE(n_components=2, random_state=0, n_iter=10000, perplexity=2)
+    np.set_printoptions(suppress=True)
+    T = tsne.fit_transform(wvs)
+    labels = words
+
+    plt.figure(figsize=(14, 8))
+    plt.scatter(T[:, 0], T[:, 1], c='orange', edgecolors='r')
+    for label, x, y in zip(labels, T[:, 0], T[:, 1]):
+        plt.annotate(label, xy=(x+1, y+1), xytext=(0, 0), textcoords='offset points')
+
+
+if __name__ == "__main__" :
+    docs = ["natural language processing and machine learning is fun and exciting",
+            "natural language processing and machine learning"]
+    
+    sg = Word2Vec()
+    wv = sg.embedding(docs, epoch=1000)
+    print(sg.wv_['natural'])
+    print(sg.most_similar('natural'))
+    sg.visualize('natural', 8)
--- 05_Prac_CNN.py
+++ 05_Prac_CNN.py
@@ -1,46 +1,51 @@
 import numpy as np
 
-def ReLU() :
-    pass
 
-def convolution(images, n_F, F, S, P=0) :
-  weights = [np.random.randn(F[0], F[1]) for i in range(n_F)]
+class CNN() :
+    def ReLU(self) :
+        pass
 
-  h = int((images[0].shape[0]-F[0]+2*P)/S[0]+1)
-  w = int((images[0].shape[1]-F[1]+2*P)/S[1]+1)
+    def convolution(self, images, n_F, F, S, P=0) :
+        weights = [np.random.randn(F[0], F[1]) for i in range(n_F)]
 
-  map = np.zeros((n_F, h, w))
+        h = int((images[0].shape[0]-F[0]+2*P)/S[0]+1)
+        w = int((images[0].shape[1]-F[1]+2*P)/S[1]+1)
 
-  #(W−F+2P)/S+1
-  for i, weight in enumerate(weights) :
-    for img in images :
-      for y in range(0, h, S[0]) :
-        for x in range(0, w, S[1]) :
-          tmp = img[y:y+F[0], x:x+F[1]][:,:,0]
-          map[i, y, x] = np.sum(np.multiply(weight, tmp))
-  return map
+        map = np.zeros((n_F, h, w))
 
-def maxpooling(map, pooling) :
-  h = int(map[0].shape[0]/pooling[0])
-  w = int(map[0].shape[1]/pooling[1])
+        #(W−F+2P)/S+1
+        for i, weight in enumerate(weights) :
+            for img in images :
+                for y in range(0, h, S[0]) :
+                    for x in range(0, w, S[1]) :
+                        tmp = img[y:y+F[0], x:x+F[1]][:,:,0]
+                        map[i, y, x] = np.sum(np.multiply(weight, tmp))
+        return map
 
-  mat = np.zeros((map.shape[0], h, w))
+    def maxpooling(self, map, pooling) :
+        h = int(map[0].shape[0]/pooling[0])
+        w = int(map[0].shape[1]/pooling[1])
 
-  for i in range(map.shape[0]) :
-    for y in range(0, h, pooling[0]) :
-      for x in range(0, w, pooling[1]) :
-        #print(map[i,y:y+pooling[0],x:x+pooling[1]])
-        mat[i, y, x] = np.max(map[i,y:y+pooling[0],x:x+pooling[1]])
+        mat = np.zeros((map.shape[0], h, w))
 
-  return mat
+        for i in range(map.shape[0]) :
+            for y in range(0, h, pooling[0]) :
+                for x in range(0, w, pooling[1]) :
+                    mat[i, y, x] = np.max(map[i,y:y+pooling[0],x:x+pooling[1]])
 
-def fullyconected() :
-  pass
+        return mat
+
+    def fullyconected(self, ) :
+        pass
 
 
-def CNN(data, label) :
-    pass
-  #convolution()
+    def train(self, data, layes) :
+        
+        
+        for layer in layers :
+
+
+    #convolution()
 
 
 if __name__ == "__main__" :
@@ -54,8 +59,20 @@ if __name__ == "__main__" :
     # 픽셀 값을 0~1 사이로 정규화합니다.
     train_images, test_images = train_images / 255.0, test_images / 255.0
 
-    conv1 = convolution([train_images[:2]], n_F=32, F=(3, 3), S=(1, 1))
-    pooling1 = maxpooling(conv1, (2,2))
+    cnn = CNN()
+    cnn.train([train_images[:2]], 
+        [
+            {'layer':'CONV','n_F':32, 'F':(3,3), 'S':(1,1)},
+            {'layer':'POOL','pooling':(2,2)},
+            {'layer':'CONV','n_F':64, 'F':(3,3), 'S':(1,1)},
+            {'layer':'POOL','pooling':(2,2)}
+            {'layer':'FLAT'}
+            {'layer':'FC','dense':64}
+            {'layer':'FC','dense':10}
+        ])
 
-    conv2 = convolution(conv1, n_F=64, F=(3, 3), S=(1, 1))
-    pooling2 = maxpooling(conv2, (2,2))
+    # conv1 = convolution([train_images[:2]], n_F=32, F=(3, 3), S=(1, 1))
+    # pooling1 = maxpooling(conv1, (2,2))
+
+    # conv2 = convolution(conv1, n_F=64, F=(3, 3), S=(1, 1))
+    # pooling2 = maxpooling(conv2, (2,2))
